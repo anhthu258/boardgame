802ec9fbb3f53256bc4619f6de29c450fa5a4dcd
Mapou435 <eaa24mkrp@students.eaaa.dk>
2025-04-01 13:53:31 +0200

    Update til java Time

--- javascript/time.js
+++ javascript/time.js
@@ -3,3 +3,15 @@
 const Minutes = document.getElementById('minutes');
 const Seconds = document.getElementById('seconds');
 const ResetButton = document.getElementById('resetButton');
+
+let timerInterval;
+let totalTime = 10 * 60;
+
+function updateTimerDisplay() {
+    const minutes = Math.floor(totalTime / 60);
+    const seconds = totalTime % 60;
+
+    Minutes.textContent = String(minutes).padStart(2, '0');
+    Seconds.textContent = String(seconds).padStart(2, '0');
+}
+
